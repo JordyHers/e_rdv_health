b3205acfb40a685f9746aa8c9f722d324c649284
jordyhers <jordy925@gmail.com>
2021-11-06 19:54:11 +0300

    integration_test_driver.dart added. All test passed/ and custom drawer

--- lib/service/database/http_request.dart
+++ lib/service/database/http_request.dart
@@ -4,14 +4,13 @@ abstract class HttpReq {
   Future<String?> getApiListOfHospitals() async {}
 }
 
-const URL = 'https://example.com/whatsit/create';
+const URL = 'http://erendezvoussante.online/api/cliniques';
 
 class HttpRequest extends HttpReq {
   @override
   Future<String?> getApiListOfHospitals() async {
     var url = Uri.parse(URL);
-    var response =
-        await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+    var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
   }
